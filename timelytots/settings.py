2b9f94ee534f09027c7fb95a34513ba3b2d93345
Rayyan Shaikh <rayyanshaikh@192.168.1.8>
2025-09-26 06:01:31 -0400

    Updated scheduler

--- timelytots/settings.py
+++ timelytots/settings.py
@@ -126,9 +126,16 @@ SIMPLE_JWT = {
 
 MSG91_AUTH_KEY = "467566A1bf8qD568c2b9efP1"
 
+# Redis broker (already exists)
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+# Celery result backend
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata' 
 
 CELERY_BEAT_SCHEDULE = {
     'send-reminders-every-day': {
