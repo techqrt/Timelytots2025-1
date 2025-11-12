7b31632767baba1a0862700697e9b33c9eb6ce46
Rayyan Shaikh <rayyanshaikh@192.168.1.5>
2025-11-12 17:54:41 +0400

    Updated the Notification

--- doctorApp/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import firebase_utils
--- doctorApp/apps.py
+++ doctorApp/apps.py
@@ -5,3 +5,8 @@ class DoctorappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "doctorApp"
     verbose_name = 'Doctors'
+
+    def ready(self):
+        # ✅ Import all Celery task modules here safely
+        import doctorApp.firebase_utils
+        import doctorApp.utils
